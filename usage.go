b0d37d1fb2b491a861f788a41aabbe66cbf83110
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2015-11-19 14:34:09 +0100

    Add default values to usage
    Check if the value isn't it's zero value and if not add a default value
    to the usage text.

--- usage.go
+++ usage.go
@@ -76,7 +76,6 @@ func (p *Parser) WriteHelp(w io.Writer) {
 		}
 	}
 
-	// write the list of options
 	fmt.Fprint(w, "\noptions:\n")
 	for _, spec := range options {
 		printOption(w, spec)
@@ -100,6 +99,13 @@ func printOption(w io.Writer, spec *spec) {
 			fmt.Fprint(w, "\n"+strings.Repeat(" ", colWidth))
 		}
 		fmt.Fprint(w, spec.help)
+	}
+	// Check if spec.dest is zero value or not
+	// If it isn't a default value have been added
+	v := spec.dest
+	z := reflect.Zero(v.Type())
+	if v.Interface() != z.Interface() {
+		fmt.Fprintf(w, " [default: %v]", v)
 	}
 	fmt.Fprint(w, "\n")
 }
