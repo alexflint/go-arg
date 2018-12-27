f8987d11059a6a868a8e179832e7dfaeaac854f0
Alex Flint <alex.flint@gmail.com>
2018-12-27 11:28:34 -0800

    hook integration tests into travis

--- test/compile_with_go110.sh
+++ test/compile_with_go110.sh
@@ -4,9 +4,11 @@
 # under go 1.10, which was the last release before introduction of the new go
 # module system.
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 docker run \
     --rm \
-    -v $(pwd)/some-program:/src \
+    -v $DIR/some-program:/src \
     -w /src \
     golang:1.10 \
     bash -c "go get github.com/alexflint/go-arg && go build -o /dev/null"
