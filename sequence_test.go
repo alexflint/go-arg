0100c0a411486a0a26c0d7bb5504c5f371aaf6b0
Alex Flint <alex.flint@gmail.com>
2021-04-19 13:48:07 -0700

    push code coverage up

--- sequence_test.go
+++ sequence_test.go
@@ -16,12 +16,19 @@ func TestSetSliceWithoutClearing(t *testing.T) {
 	assert.Equal(t, []int{10, 1, 2, 3}, xs)
 }
 
-func TestSetSliceWithClear(t *testing.T) {
+func TestSetSliceAfterClearing(t *testing.T) {
 	xs := []int{100}
 	entries := []string{"1", "2", "3"}
 	err := setSlice(reflect.ValueOf(&xs).Elem(), entries, true)
 	require.NoError(t, err)
 	assert.Equal(t, []int{1, 2, 3}, xs)
+}
+
+func TestSetSliceInvalid(t *testing.T) {
+	xs := []int{100}
+	entries := []string{"invalid"}
+	err := setSlice(reflect.ValueOf(&xs).Elem(), entries, true)
+	assert.Error(t, err)
 }
 
 func TestSetSlicePtr(t *testing.T) {
@@ -58,7 +65,7 @@ func TestSetMapWithoutClearing(t *testing.T) {
 	assert.Equal(t, 10, m["foo"])
 }
 
-func TestSetMapWithClear(t *testing.T) {
+func TestSetMapAfterClearing(t *testing.T) {
 	m := map[string]int{"foo": 10}
 	entries := []string{"a=1", "b=2"}
 	err := setMap(reflect.ValueOf(&m).Elem(), entries, true)
@@ -66,6 +73,25 @@ func TestSetMapWithClear(t *testing.T) {
 	require.Len(t, m, 2)
 	assert.Equal(t, 1, m["a"])
 	assert.Equal(t, 2, m["b"])
+}
+
+func TestSetMapWithKeyPointer(t *testing.T) {
+	// textUnmarshaler is a struct that captures the length of the string passed to it
+	var m map[*string]int
+	entries := []string{"abc=123"}
+	err := setMap(reflect.ValueOf(&m).Elem(), entries, true)
+	require.NoError(t, err)
+	require.Len(t, m, 1)
+}
+
+func TestSetMapWithValuePointer(t *testing.T) {
+	// textUnmarshaler is a struct that captures the length of the string passed to it
+	var m map[string]*int
+	entries := []string{"abc=123"}
+	err := setMap(reflect.ValueOf(&m).Elem(), entries, true)
+	require.NoError(t, err)
+	require.Len(t, m, 1)
+	assert.Equal(t, 123, *m["abc"])
 }
 
 func TestSetMapTextUnmarshaller(t *testing.T) {
@@ -78,6 +104,20 @@ func TestSetMapTextUnmarshaller(t *testing.T) {
 	assert.Equal(t, &textUnmarshaler{3}, m[textUnmarshaler{1}])
 	assert.Equal(t, &textUnmarshaler{2}, m[textUnmarshaler{2}])
 	assert.Equal(t, &textUnmarshaler{1}, m[textUnmarshaler{3}])
+}
+
+func TestSetMapInvalidKey(t *testing.T) {
+	var m map[int]int
+	entries := []string{"invalid=123"}
+	err := setMap(reflect.ValueOf(&m).Elem(), entries, true)
+	assert.Error(t, err)
+}
+
+func TestSetMapInvalidValue(t *testing.T) {
+	var m map[int]int
+	entries := []string{"123=invalid"}
+	err := setMap(reflect.ValueOf(&m).Elem(), entries, true)
+	assert.Error(t, err)
 }
 
 func TestSetMapMalformed(t *testing.T) {
